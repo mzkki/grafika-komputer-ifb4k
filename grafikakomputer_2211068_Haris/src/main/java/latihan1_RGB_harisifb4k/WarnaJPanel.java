24d55615f3d8b598259ada2aa00040e6f8b6e7b4
mzkki <08harismu@gmail.com>
2024-04-25 12:54:00 +0800

    chore(color): change the color

--- grafikakomputer_2211068_Haris/src/main/java/latihan1_RGB_harisifb4k/WarnaJPanel.java
+++ grafikakomputer_2211068_Haris/src/main/java/latihan1_RGB_harisifb4k/WarnaJPanel.java
@@ -18,22 +18,22 @@ public class WarnaJPanel extends JPanel{
         
         this.setBackground(Color.WHITE);
         
-        g.setColor(new Color(255,0,0));
+        g.setColor(new Color(150,250,15));
         g.fillRect(15,25,100,20);
         g.drawString("RGB Sekarang: " + g.getColor(), 130, 40);
         
         
-        g.setColor(new Color(0.50f, 0.75f, 0.0f));
+        g.setColor(new Color(0.75f, 0.75f, 1.0f));
         g.fillRect(15,50,100,20);
         g.drawString("RGB Sekarang: " + g.getColor(), 130, 65);
         
-        g.setColor(Color.BLUE);
+        g.setColor(Color.PINK);
         g.fillRect(15,75,100,20);
         g.drawString("RGB Sekarang: " + g.getColor(), 130, 90);
         
-        Color warna = Color.MAGENTA;
+        Color warna = Color.GRAY;
         g.setColor(warna);
         g.fillRect(15,100,100,20);
-        g.drawString("Nilai-nilai RGB: " + warna.getRed() + ", " + warna.getGreen() + ", "+ warna.getBlue(), 130, 115);
+        g.drawString("Nilai-nilai RGB: r=" + warna.getRed() + ", g=" + warna.getGreen() + ", b="+ warna.getBlue(), 130, 115);
     }
 }
