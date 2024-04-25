f6be1a7bb4477597100cb7aead2fe0a786e94bfd
mzkki <08harismu@gmail.com>
2024-04-25 09:37:36 +0800

    chore(class): create class

---
+++ grafikakomputer_2211068_Haris/src/main/java/latihan1_RGB_harisifb4k/TampilWarna.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan1_RGB_harisifb4k;
+
+/**
+ *
+ * @author mzkki
+ */
+public class TampilWarna {
+    
+}
--- grafikakomputer_2211068_Haris/src/main/java/latihan1_RGB_harisifb4k/WarnaJPanel.java
+++ grafikakomputer_2211068_Haris/src/main/java/latihan1_RGB_harisifb4k/WarnaJPanel.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package latihan1_RGB_harisifb4k;
 
 /**
  *
  * @author mzkki
  */
-public class Grafikakomputer_2211068_Haris {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class WarnaJPanel {
+    
 }
