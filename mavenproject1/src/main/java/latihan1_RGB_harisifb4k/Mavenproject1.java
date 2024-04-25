28083cfc97ec6e2c8adb378e0b86700df3c6a360
mzkki-campus <08harismu@gmail.com>
2024-04-25 09:24:18 +0800

    initial commit

---
+++ mavenproject1/src/main/java/latihan1_RGB_harisifb4k/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package latihan1_RGB_harisifb4k;
+
+/**
+ *
+ * @author mzkki
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
