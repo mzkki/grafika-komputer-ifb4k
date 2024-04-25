4ffc71c0d4d22c8f18379d1cd8b7be9d35a04430
mzkki <08harismu@gmail.com>
2024-04-25 09:54:59 +0800

    feat(rgb): create the frame and panel

--- grafikakomputer_2211068_Haris/src/main/java/latihan1_RGB_harisifb4k/TampilWarna.java
+++ grafikakomputer_2211068_Haris/src/main/java/latihan1_RGB_harisifb4k/TampilWarna.java
@@ -4,10 +4,21 @@
  */
 package latihan1_RGB_harisifb4k;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author mzkki
  */
 public class TampilWarna {
-    
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Menggunakan Warna");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        WarnaJPanel warnaJPanel = new WarnaJPanel();
+        frame.add(warnaJPanel);
+        frame.setSize(500,200);
+        frame.setVisible(true);
+    }
 }
