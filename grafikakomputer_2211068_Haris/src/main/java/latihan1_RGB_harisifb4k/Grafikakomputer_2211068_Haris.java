b87e1f73254a7d6d95e55b7948515ab316c3b555
mzkki-campus <08harismu@gmail.com>
2024-04-25 09:33:43 +0800

    change project name

---
+++ grafikakomputer_2211068_Haris/src/main/java/latihan1_RGB_harisifb4k/Grafikakomputer_2211068_Haris.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package latihan1_RGB_harisifb4k;
+
+/**
+ *
+ * @author mzkki
+ */
+public class Grafikakomputer_2211068_Haris {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
